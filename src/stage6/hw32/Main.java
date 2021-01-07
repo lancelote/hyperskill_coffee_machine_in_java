fffd1f47a423db9617786b95858ebd7baffc671f
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-01-07 15:03:56 +0300

    Stage 6 homework 32

---
+++ src/stage6/hw32/Main.java
@@ -0,0 +1,20 @@
+package stage6.hw32;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static long factorial(long n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = Integer.parseInt(scanner.nextLine().trim());
+        System.out.println(factorial(n));
+    }
+}
